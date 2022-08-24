1fee2f534248821392c0f50f48d0d52ee0619d13
cceph <yousefelsisyy@gmail.com>
2022-08-24 13:03:08 -0300

    Add a range slider and display its value
    Note that this slider will be used by the user to select the size of the
    grid. However, there is no code that allows it to do that yet.

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 function createGrid() {
     grid = document.querySelector(".grid");
-for (let i = 0; i <= 16; i++) {
-    gridRow = document.createElement('div');
-    gridRow.classList.add('row');
-    gridRow.style.cssText = "flex: auto; display: flex;";
     for (let i = 0; i <= 16; i++) {
-        gridSquare = document.createElement('div');
-        gridSquare.classList.add('square');
-        enableHoverColor(gridSquare)
-        gridSquare.style.cssText = "flex: auto; border-style: solid; border-color: black; border-width: thin;"
-        gridRow.appendChild(gridSquare);
+        gridRow = document.createElement('div');
+        gridRow.classList.add('row');
+        gridRow.style.cssText = "flex: auto; display: flex;";
+        for (let i = 0; i <= 16; i++) {
+            gridSquare = document.createElement('div');
+            gridSquare.classList.add('square');
+            enableHoverColor(gridSquare)
+            gridSquare.style.cssText = "flex: auto; border-style: solid; border-color: black; border-width: thin;"
+            gridRow.appendChild(gridSquare);
+        }
+        grid.appendChild(gridRow);
     }
-    grid.appendChild(gridRow);
-}
 }
 
 
 function enableHoverColor(gridSquare) {
     gridSquare.addEventListener('mouseenter', () =>
-    gridSquare.classList.add('activated'))
+    gridSquare.classList.add('activated'));
+}
+
+function displayGridSize() {
+    let slider = document.getElementById("gridSlider")
+    let gridSize = document.getElementById("gridSize")
+    gridSize.textContent = `${slider.value}x${slider.value}` //displays the default grid size.
+
+    slider.oninput = () => gridSize.textContent = `${slider.value}x${slider.value}`;
 }
 
 function main() {
     createGrid();
+    displayGridSize();
 }
 
 main();
